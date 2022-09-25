3b8fe58093816abe6429e677d76ebf5e60d5283f
Elias Roberts <elias@eroberts.dev>
2022-09-25 11:33:38 -0600

    map added to footer

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+document.body.onscroll = function myFunction() {
+	let scrolltotop = document.scrollingElement.scrollTop;
+	let target = document.getElementById('about-2');
+	let xvalue = 'bottom';
+	let factor = 0.5;
+	let yvalue = scrolltotop * factor;
+	target.style.backgroundPosition = xvalue + ' ' + yvalue + 'px';
+};
