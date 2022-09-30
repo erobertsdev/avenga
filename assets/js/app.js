cd980a6cba17985f7b0604ef67f6c083f252a968
Elias Roberts <elias@eroberts.dev>
2022-09-30 15:02:27 -0600

    upper half of contact widget complete

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+// Contact Widget
+// -----------------------------------
+const contactWidget = document.getElementById('contact-widget');
+contactWidget.addEventListener('click', () => {
+	// Hides small widget when clicked
+	contactWidget.style.bottom = '-200px';
+});
