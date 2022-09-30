829f2092ef9d8a03a7a8e6c8d6c36824ca202bc4
Elias Roberts <elias@eroberts.dev>
2022-09-30 15:40:16 -0600

    finished contact widget

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,21 @@
 // Contact Widget
 // -----------------------------------
-const contactWidget = document.getElementById('contact-widget');
+const contactWidget = document.getElementById('contact-widget'),
+	contactWidgetOpen = document.getElementById('contact-widget--open'),
+	contactWidgetClose = document.getElementById('contact-widget--close');
+
+// Opens main widget when clicked
 contactWidget.addEventListener('click', () => {
 	// Hides small widget when clicked
 	contactWidget.style.bottom = '-200px';
+	// Shows full widget
+	contactWidgetOpen.style.bottom = '0';
+});
+
+// Closes main widget when clicked and shows small widget
+contactWidgetClose.addEventListener('click', () => {
+	// Hides full widget
+	contactWidgetOpen.style.bottom = '-380px';
+	// Shows small widget
+	contactWidget.style.bottom = '0';
 });
